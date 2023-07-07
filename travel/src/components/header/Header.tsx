a742bfbb0a3a1be3342b601fbe20f5739a38059c
JasonNieh <45752751+JasonNieh@users.noreply.github.com>
2023-07-06 20:17:17 -0700

    Added react-redux support for Header Functional component. Used useSelector Hook and customzied it with TypedUseSelectorHook to add RootState to this selector hook.

--- travel/src/components/header/Header.tsx
+++ travel/src/components/header/Header.tsx
@@ -3,30 +3,52 @@ import logo from "../../assets/images/logo.svg";
 import styles from "./Header.module.css";
 import { Typography, Layout, Input, Menu, Button, Dropdown } from 'antd';
 import { GlobalOutlined } from '@ant-design/icons';
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "../../redux/hooks";
+import { LanguageActionTypes, addLanguageCreator, changeLanguageCreator } from "../../redux/language/languageActions";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+
 
 export const Header: React.FC = () => {
+    const params = useParams();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const language = useSelector(state => state.language);
+    const languageList = useSelector(state => state.languageList);
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+
+    const menuHandleClick = (e) => {
+        if (e.key === "new") {
+            dispatch(addLanguageCreator("New Language", "new_lang"));
+        }
+        else {
+            dispatch(changeLanguageCreator(e.key));
+        }
+    }
     return (
         <div className={styles['app-header']}>
             <div className={styles['top-header']}>
                 <div className={styles.inner}>
-                    <Typography.Text style={{ color: 'grey' }}>让旅游更幸福</Typography.Text>
+                    <Typography.Text style={{ color: 'grey' }}>{t("header.slogan")}</Typography.Text>
                     <Dropdown.Button
                         style={{ marginLeft: 15 }}
                         overlay={
-                            <Menu
+                            <Menu onClick={menuHandleClick}
                                 items={[
-                                    { key: "1", label: "中文" },
-                                    { key: "2", label: "English" },
+                                    ...languageList.map(l => { return { key: l.code, label: l.name }; }),
+                                    { key: "new", label: t("header.add_new_language") }
                                 ]}
                             />
                         }
                         icon={<GlobalOutlined />}
                     >
-                        Language
+                        {language === "zh" ? "中文" : "English"}
                     </Dropdown.Button>
                     <Button.Group className={styles['button-group']}>
-                        <Button>注册</Button>
-                        <Button>登录</Button>
+                        <Button onClick={() => navigate("./register")}>{t("header.register")}</Button>
+                        <Button onClick={() => navigate("./signin")}>{t("header.signin")}</Button>
                     </Button.Group>
                 </div>
             </div>
@@ -34,28 +56,28 @@ export const Header: React.FC = () => {
             <Layout.Header className={styles['main-header']}>
                 <img src={logo} alt="" className={styles["App-logo"]} />
                 <Typography.Title level={3} className={styles.title}>React Travel</Typography.Title>
-                <Input.Search placeholder='请输入旅游目的地、主题或关键字' className={styles['search-input']} />
+                <Input.Search placeholder={t("header.search_place_holder")} className={styles['search-input']} />
             </Layout.Header>
             <Menu
                 mode={"horizontal"}
                 className={styles["main-menu"]}
                 items={[
-                    { key: "1", label: "旅游首页" },
-                    { key: "2", label: "周末游" },
-                    { key: "3", label: "跟团游" },
-                    { key: "4", label: "自由行" },
-                    { key: "5", label: "私家团" },
-                    { key: "6", label: "邮轮" },
-                    { key: "7", label: "酒店+景点" },
-                    { key: "8", label: "当地玩乐" },
-                    { key: "9", label: "主题游" },
-                    { key: "10", label: "定制游" },
-                    { key: "11", label: "游学" },
-                    { key: "12", label: "签证" },
-                    { key: "13", label: "企业游" },
-                    { key: "14", label: "高端游" },
-                    { key: "15", label: "爱玩户外" },
-                    { key: "16", label: "保险" },
+                    { key: "1", label: t("header.home_page") },
+                    { key: "2", label: t("header.weekend") },
+                    { key: "3", label: t("header.group") },
+                    { key: "4", label: t("header.backpack") },
+                    { key: "5", label: t("header.private") },
+                    { key: "6", label: t("header.cruise") },
+                    { key: "7", label: t("header.hotel") },
+                    { key: "8", label: t("header.local") },
+                    { key: "9", label: t("header.theme") },
+                    { key: "10", label: t("header.custom") },
+                    { key: "11", label: t("header.study") },
+                    { key: "12", label: t("header.visa") },
+                    { key: "13", label: t("header.enterprise") },
+                    { key: "14", label: t("header.high_end") },
+                    { key: "15", label: t("header.outdoor") },
+                    { key: "16", label: t("header.insurance") },
                 ]}
             />
 
--- travel/src/components/header/index.ts
+++ travel/src/components/header/index.ts
@@ -1,1 +1,3 @@
-export * from './Header.class';
+// export * from './Header.class';
+export * from './Header';
+
